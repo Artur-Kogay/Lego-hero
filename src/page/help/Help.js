eff615264a53536aefe37512b075c4290769623a
Baiaman Kodirov <baiamankodirov@MacBook-Pro-Baiaman.local>
2022-02-24 16:36:59 +0600

    add accordion component

--- src/page/help/Help.js
+++ src/page/help/Help.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import MainLayout from '../../components/MainLayout/index'
-
+import HelpAccordion from '../../components/HelpAccordion/HelpAccordion';
+import MainLayout from '../../components/MainLayout/index';
+import styles from "./help.module.scss";
+import humanIcon from "../../img/contactHuman.svg";
 const Help = () => {
     return (
         <MainLayout>
-            <h1>help</h1>
+            <main className={styles.container}>
+                <div className={styles.contact__block}>
+                        <HelpAccordion title={"Вопросы к организаторам"} img={humanIcon} icon={humanIcon} backgroundColor={"linear-gradient(266.95deg, #982EEB -20.2%, #359AD2 132.4%), #151515"}>
+                            Content
+                        </HelpAccordion>
+                </div>
+            </main>
         </MainLayout>
     );
 };
