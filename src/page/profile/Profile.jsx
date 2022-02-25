1ee7995cc86b5bff2e48cc86182b79ff127d8c0a
WhiteHanClean <whitehanclean@gmail.com>
2022-02-25 09:33:35 +0300

    bugfix/profile

--- src/page/profile/Profile.jsx
+++ src/page/profile/Profile.jsx
@@ -1,70 +1,147 @@
-import React from 'react';
-import MainLayout from '../../components/MainLayout';
-import cl from "./profile.module.scss"
-import account from '../../img/loll.png'
+import React, { useState } from "react";
+import MainLayout from "../../components/MainLayout";
+import cl from "./profile.module.scss";
+import account from "../../img/loll.png";
+// import ProcessBare from "../../components/proccesBare/ProcessBare";
 
 const Profile = () => {
-    return (
-        <MainLayout>
-            <div className={cl.profile}>
+  const profile = {
+    name: "Александр Иванов",
+    data: "9 мая 1990",
+    telephon: "+7 985 778 88 77",
+    city: "Липецк",
+    typeProfile: "Начальный",
+    email: "ivanov@yandex.ru",
+  };
+  const [name, setName] = useState(profile);
 
-            <h2 className={cl.title}>ЛИЧНЫЙ КАБИНЕТ</h2>
-            <div className={cl.prof_block}>
-                <div className={cl.photo}>
-                <img className={cl.pho} src={account} alt='lol'/>
-                </div>
+  const [active, setActive] = useState(false);
 
-                <div className={cl.profile_detail}>
+  const handleChange = () => {
+    const profile = {
+      name: "ok",
+      data: "9 мая 1990",
+      telephon: "+7 985 778 88 77",
+      city: "Липецк",
+      typeProfile: "Начальный",
+      email: "ivanov@yandex.ru",
+    };
+  };
 
-                    <div className={cl.ok}>
-                        <div className={cl.min_block}>
-                            <p>Ваше имя и фамилия</p>
-                            <h5>Александр Иванов</h5>
-                        </div>
-                        <div className={cl.min_block}>
-                            <p>Дата рождения</p>
-                            <h5>9 мая 1990</h5>
-                        </div>
-                        <div className={cl.min_block}>
-                            <p>Телефон</p>
-                            <h5>+7 985 778 88 77</h5>
-                        </div>
-                    </div>
+  return (
+    <MainLayout>
+      <div className={cl.profile}>
+        <h2 className={cl.title}>ЛИЧНЫЙ КАБИНЕТ</h2>
+        <div className={cl.prof_block}>
+          <div className={cl.photo}>
+            <img className={cl.pho} src={account} alt="lol" />
+          </div>
 
-                    <div className="ok">
-                        <div className={cl.min_block}>
-                            <p>Город</p>
-                            <h5>Липецк</h5>
-                        </div>
-                        <div className={cl.min_block}>
-                            <p>Тип профиля</p>
-                            <h5>Начальный</h5>
-                        </div>
-                        <div className={cl.min_block}>
-                            <p>E-mail</p>
-                            <h5>ivanov@yandex.ru</h5>
-                        </div>
-                    </div>
-                    <div>
-                        <button className={cl.btn_profie}>
-                           Изменить
-                        </button>
-                    </div>
-                    
-                </div>
-
-                <div className={cl.profile_cashh}>
-                <div className={cl.profile_cash}></div>
-
-                </div>
-
+          <div className={cl.profile_detail}>
+            <div className={cl.ok}>
+              <div className={cl.min_block}>
+                <p>Ваше имя и фамилия</p>
+                {active === false ? (
+                  <h5>{name.name}</h5>
+                ) : (
+                  <input
+                    type="text"
+                    value={name.name}
+                    onChange={(e) => {
+                      console.log(e.target.value);
+                      setName(name.name.e.target.value);
+                    }}
+                  />
+                )}
+              </div>
+              <p>Дата рождения</p>
+              <div className={cl.min_block}>
+                {active === false ? (
+                  <h5>{name.data}</h5>
+                ) : (
+                  <input
+                    type="text"
+                    value={name.data}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                )}
+              </div>
+              <div className={cl.min_block}>
+                <p>Телефон</p>
+                {active === false ? (
+                  <h5>{name.telephon}</h5>
+                ) : (
+                  <input
+                    type="text"
+                    value={name.telephon}
+                    onChange={(e) =>
+                      setName({
+                        telephon: e.target.value,
+                      })
+                    }
+                  />
+                )}
+              </div>
             </div>
 
-            <hr style={{marginTop:'57px'}}/>
-
+            <div className="ok">
+              <div className={cl.min_block}>
+                <p>Город</p>
+                {active === false ? (
+                  <h5>{name.city}</h5>
+                ) : (
+                  <input
+                    type="text"
+                    value={name.city}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                )}
+              </div>
+              <div className={cl.min_block}>
+                <p>Тип профиля</p>
+                {active === false ? (
+                  <h5>{name.typeProfile}</h5>
+                ) : (
+                  <input
+                    type="text"
+                    value={name.typeProfile}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                )}
+              </div>
+              <div className={cl.min_block}>
+                <p>E-mail</p>
+                {active === false ? (
+                  <h5>{name.email}</h5>
+                ) : (
+                  <input
+                    type="text"
+                    value={name.email}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                )}
+              </div>
             </div>
-        </MainLayout>
-    );
+            <div>
+              {active === false ? (
+                <button className={cl.btn_profie}>Изменить</button>
+              ) : (
+                <button className={cl.btn_profie}>Сохранить</button>
+              )}
+              {/* onClick={() => setActive(false)} */}
+            </div>
+          </div>
+
+          <div className={cl.profile_cashh}>
+            <div className={cl.profile_cash}></div>
+          </div>
+        </div>
+
+        <hr style={{ marginTop: "57px" }} />
+
+      </div>
+    </MainLayout>
+  );
 };
 
 export default Profile;
