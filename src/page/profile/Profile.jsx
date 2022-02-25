4b21476c6e3b87474f1adea4bb4c5284bd4b93d5
Mirlan <email>
2022-02-25 19:18:01 +0600

    add_lk

--- src/page/profile/Profile.jsx
+++ src/page/profile/Profile.jsx
@@ -1,70 +1,97 @@
-import React from 'react';
-import MainLayout from '../../components/MainLayout';
-import cl from "./profile.module.scss"
-import account from '../../img/loll.png'
+import React, { useEffect } from "react";
+import MainLayout from "../../components/MainLayout";
+import {useMediaQuery} from "@mui/material";
+import cl from "./profile.module.scss";
+import account from "../../img/loll.png";
+import fotka from "../../img/lolll.png";
 
 const Profile = () => {
-    return (
-        <MainLayout>
-            <div className={cl.profile}>
+  const isLaptopMax = useMediaQuery("(max-width:768px)");
+  const isLaptopMin = useMediaQuery("(min-width: 468px)")
 
-            <h2 className={cl.title}>ЛИЧНЫЙ КАБИНЕТ</h2>
-            <div className={cl.prof_block}>
-                <div className={cl.photo}>
-                <img className={cl.pho} src={account} alt='lol'/>
-                </div>
+  useEffect(() => {}, []);
 
-                <div className={cl.profile_detail}>
+  return (
+    <MainLayout>
+      <div className={cl.profile}>
+        <div className={cl.titleWrapper}>
+          <h2 className={cl.title}>ЛИЧНЫЙ КАБИНЕТ</h2>
+          {(isLaptopMax && isLaptopMin) && <button className={cl.btn_profie}>Изменить</button>}
+        </div>
+        <div className={cl.prof_block}>
+          <div className={cl.photo}>
+            <img className={cl.pho} src={account} alt="lol" />
+          </div>
 
-                    <div className={cl.ok}>
-                        <div className={cl.min_block}>
-                            <p>Ваше имя и фамилия</p>
-                            <h5>Александр Иванов</h5>
-                        </div>
-                        <div className={cl.min_block}>
-                            <p>Дата рождения</p>
-                            <h5>9 мая 1990</h5>
-                        </div>
-                        <div className={cl.min_block}>
-                            <p>Телефон</p>
-                            <h5>+7 985 778 88 77</h5>
-                        </div>
-                    </div>
-
-                    <div className="ok">
-                        <div className={cl.min_block}>
-                            <p>Город</p>
-                            <h5>Липецк</h5>
-                        </div>
-                        <div className={cl.min_block}>
-                            <p>Тип профиля</p>
-                            <h5>Начальный</h5>
-                        </div>
-                        <div className={cl.min_block}>
-                            <p>E-mail</p>
-                            <h5>ivanov@yandex.ru</h5>
-                        </div>
-                    </div>
-                    <div>
-                        <button className={cl.btn_profie}>
-                           Изменить
-                        </button>
-                    </div>
-                    
-                </div>
-
-                <div className={cl.profile_cashh}>
-                <div className={cl.profile_cash}></div>
-
-                </div>
-
+          <div className={cl.profile_detail}>
+            <div className={cl.ok}>
+              <div className={cl.min_block}>
+                <p>Ваше имя и фамилия</p>
+                <h5>Александр Иванов</h5>
+              </div>
+              <div className={cl.min_block}>
+                <p>Дата рождения</p>
+                <h5>9 мая 1990</h5>
+              </div>
+              <div className={cl.min_block}>
+                <p>Телефон</p>
+                <h5>+7 985 778 88 77</h5>
+              </div>
             </div>
 
-            <hr style={{marginTop:'57px'}}/>
-
+            <div className="ok">
+              <div className={cl.min_block}>
+                <p>Город</p>
+                <h5>Липецк</h5>
+              </div>
+              <div className={cl.min_block}>
+                <p>Тип профиля</p>
+                <h5>Начальный</h5>
+              </div>
+              <div className={cl.min_block}>
+                <p>E-mail</p>
+                <h5>ivanov@yandex.ru</h5>
+              </div>
             </div>
-        </MainLayout>
-    );
+            {!(isLaptopMax && isLaptopMin) && <div>
+              <button className={cl.btn_profie}>Изменить</button>
+            </div>}
+          </div>
+
+          <div className={cl.profile_cashh}>
+            <div className={cl.profile_cash}>
+              <div className={cl.cart_portfolio}>
+                <div className={cl.cart_bud}>
+                  <div className={cl.cart_title}>
+                    <h5>«Твой кэшбэк»</h5>
+                    <p>
+                      Получайте повышенный кэшбэк на расходы в категории
+                      спортивные товары!{" "}
+                    </p>
+                  </div>
+
+                  <img
+                    className={cl.fotka}
+                    src={fotka}
+                    alt="asd"
+                    style={{
+                      width: "170px",
+                      height: "110px",
+                      marginLeft: "20px",
+                    }}
+                  />
+                </div>
+
+                <button>Узнать подробнее</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <hr style={{ marginTop: "57px" }} />
+      </div>
+    </MainLayout>
+  );
 };
 
 export default Profile;
