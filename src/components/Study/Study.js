dc68da697f6f90e64b6f3b0bab6d8b80cb99ff7a
Talgat <dzhailikeevtalgat@gmail.com>
2022-03-03 11:13:49 +0600

    add learn bar

--- src/components/Study/Study.js
+++ src/components/Study/Study.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import cls from './study.module.scss'
 import StudyAccord from './StudyAccord';
-
+import Learn from '../Learn/Learn';
 const Study = () => {
     return (
         <div className={cls.study}>
+            <Learn/>
             <StudyAccord/>
-            
         </div>
     );
 };
